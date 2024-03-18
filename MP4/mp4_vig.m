9302a5bdda51496817f3255b929f76c922543ed1
z-vig <zacharymvig@gmail.com>
2024-03-18 10:20:17 -0400

    Another commit

--- MP4/mp4_vig.m
+++ MP4/mp4_vig.m
@@ -40,8 +40,49 @@ set(gca);
 hold on;
 plot(w,P,'DisplayName','PowerSpectrum','color','k');
 xlabel('Frequency (Hz)');ylabel('Power');
+
+%q1.8
 h = spectrum.periodogram;
 ps = msspectrum(h,y,'Fs',Fs);
 plot(ps.Frequencies,ps.Data,'DisplayName','Periodogram','color','r');
 legend();
 
+%q1.9
+%{
+    The power spectrum peaks at both 30 and at 482 Hz, which correspond to the input frequency and to the sampling frequency - the input frequency. The Mean Squared spectrum only peaks at 30 Hz, but with twice the intensity.
+%}
+
+%q1.10
+yfreq2 = 40;
+y2 = sin(2*pi*yfreq2.*t);
+figure(3);subplot(2,1,1);
+plot(t,y2);title('q1.10');
+xlabel('Time (s)');ylabel('Signal');
+
+subplot(2,1,2);
+hold on;
+y2ft = fft(y2)./length(t);
+P = y2ft.*conj(y2ft);
+plot(w,P);title('q1.10 Power')
+xlabel('Frequency (Hz)');ylabel('Power');
+
+h2 = spectrum.periodogram;
+ps2 = msspectrum(h2,y2,'Fs',Fs);
+plot(ps2.Frequencies,ps2.Data,'r--');
+
+%q.11
+figure(4);subplot(2,1,1);
+y3 = y+y2;
+plot(t,y3);title('q1.11')
+xlabel('Time (s)');ylabel('Signal');
+
+subplot(2,1,2);
+hold on;
+y3ft = fft(y3)./length(t);
+P = y3ft.*conj(y3ft);
+plot(w,P);
+xlabel('Frequency (Hz)');ylabel('Power');
+
+h3 = spectrum.periodogram;
+ps3 = msspectrum(h3,y3,'Fs',Fs);
+plot(ps3.Frequencies,ps3.Data,'r--');
