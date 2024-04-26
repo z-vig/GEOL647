d48a27f7c0824320e95e1aad781e357d13111fdd
Z.M. Vig <zvig@umd.edu>
2024-04-26 08:55:32 -0400

    Completed MP9

--- MP9/vig_mp9.m
+++ MP9/vig_mp9.m
@@ -77,6 +77,44 @@ corr_coef = Cm(1,2)/(Cm(1,1)^0.5*Cm(2,2)^0.5);
 y(2) = 3.06;
 misfitL2 = @(m)sum((A*m-y) .^ 2);
 m1_outlier = fminsearch(misfitL2,m0);
+figure(2); hold on;
+plot(x,y,'k^','MarkerFaceColor','green','DisplayName','Data');
+plot(x,A*m_analytical,'DisplayName',"Old Fit","LineStyle","--");
+plot(x,A*m1_outlier,'DisplayName','L2 Fit','Color','blue');
+legend();
 %{
-    slope: 1.9405
+    slope: 1.7482, y-intercept: 0.4949
+%}
+
+%q2.2
+%{
+    By including the outlier, the slope has decreased and the y-intercept has increased. This occured because the fit is now trying to take the outlier into account.
+%}
+
+%2.3
+misfitL1 = @(m)sum(abs(A*m-y));
+m2_outlier = fminsearch(misfitL1,m0);
+plot(x,A*m2_outlier,"DisplayName","L1-norm","Color","Red");
+% slope: 1.8955, y-intercept: 0.1104
+
+%q2.4
+%{
+    Using the L1 norm, which tends to ignore outliers, the model is now much closer to the answer in part 1.
+%}
+
+%q2.5
+%{
+    The L1 and L2 norm lead to dramatically different answers because the model misfit scales as the square of the difference in the L2 norm but only as the absolute value in the L1 norm. This causes the L2 norm to be much more sensitive to outliers, one of which we now have in our data.
+%}
+
+%q2.6
+Cd(2,2) = 1;
+errorbar(x,y,diag(Cd),"LineStyle","none","DisplayName","Error");
+m_exact_outlier = ((A' * (Cd\A)) \ A') * (Cd \ y);
+plot(x,A*m_exact_outlier,"DisplayName","Analytical with Errors","Color","magenta");
+hold off;
+%slope: 1.9207, y-intercept: 0.0962
+
+%{
+    Now that the outlier point has a large amount of error, the L2 misfit function is able to ignore its contribution to the model, since it scales as the inverse of the square of the error on each data point. The model is now close to the L2 misfit of part 1 and the L1 misfit of part 2 and deviates from the L2 mistfit that did not include the error on the outlier.
 %}
