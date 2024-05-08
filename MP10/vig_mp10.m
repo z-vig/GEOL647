c5212f2ebad56e1e65fa70ca8f2a9ee8f233f447
z-vig <zacharymvig@gmail.com>
2024-05-07 21:20:52 -0400

    MP10 complete!

--- MP10/vig_mp10.m
+++ MP10/vig_mp10.m
@@ -78,16 +78,16 @@ m_corr2 = Cm_post2(1,2)/prod(sqrt(diag(Cm_post2)));
 %q2.1
 load('Data_for_Practical_10.mat');
 
-figure(3); hold on;
+figure(3);
+imagesc(X(1,:),Y(:,1),Z-1,"CDataMapping",'scaled');
+clim([-0.1,0.3]);
+colorbar();
 
-image(X(1,:),Y(:,1),Z-1,"CDataMapping",'scaled');
+hold on;
 plot(sta_x,sta_y,"rx");
-
 for i = 1:size(putanje_x,1)
     p = plot(putanje_x(i,:),putanje_y(i,:),'r');
 end
-clim([-0.1,0.3]);
-colorbar();
 hold off;
 
 %q2.2
@@ -98,29 +98,109 @@ mu = 1;
 Cm_pri = (mu^2) * eye(size(G,2));
 
 %q2.4
-figure(4);
+figure(4);subplot(1,2,1);
 m_est = (((G' * (Cd\G)) + Cm_pri) \ G') * (Cd \ d);
 % heatmap(reshape(m_est,11,11));
 image(X(1,:),Y(:,1),reshape(m_est,11,11),"CDataMapping","scaled");
 clim([-0.1,0.3]);
 colorbar();
+
+hold on;
+plot(sta_x,sta_y,"rx");
+for i = 1:size(putanje_x,1)
+    p = plot(putanje_x(i,:),putanje_y(i,:),'r');
+end
 hold off;
 %{
-    The modeled structure is certainly close to the actual structure, but differs in a few key ways. The first is that on the right angle turn at (0.8,0.2), the model struggles to reproduce the slowness values. This is expressed in the model as having a larger spread of slowness values around this area, and the slowness values that do appear are much lower than the actual structure. The top piece of the structure, however, that occurs at y=0.9 is predicted very well.
+    The modeled structure is certainly close to the actual structure, but differs in a few key ways. The first is that on the right angle turn at (0.8,0.2), the model struggles to reproduce the slowness values. This is expressed in the model as having a larger spread of slowness values around this area, and the slowness values that do appear are much lower than the actual structure. The top piece of the structure, however, that occurs at y=0.9 is predicted very well. One reason that the right angle turn may be less resolved is that this area of the grid is relatively undetermined. That is, there are only paths passing through this location coming from 2 stations.
 %}
 
 %q2.5
 Cm_pri = (0.001^2)*eye(size(G,2));
-figure(5);
+subplot(1,2,2);
 m_est2 = (((G' * (Cd\G)) + Cm_pri) \ G') * (Cd \ d);
 % heatmap(reshape(m_est2,11,11));
 image(X(1,:),Y(:,1),reshape(m_est2,11,11),"CDataMapping","scaled");
 clim([-0.1,0.3]);
 colorbar();
+hold on;
+plot(sta_x,sta_y,"rx");
+for i = 1:size(putanje_x,1)
+    p = plot(putanje_x(i,:),putanje_y(i,:),'r');
+end
 hold off;
 %{
     The retrieved structure only differs slightly from the case where mu = 1. If I had to state a general change, it looks like some vaues are now closer to the actual structure of uniform 0.2 slowness, but this is not always the case. The two cases are likely so similar because the data is not changing, only the prior information. Therefore, the model comes to roughly the same conclusion, since it is seeing the same data.
 %}
 
 %q2.6
+noise1 = 0.1*randn(size(d));
+noise2 = randn(size(d));
+d_noise1 = d+noise1;
+d_noise2 = d+noise2;
+Cm_pri = eye(size(G,2));
 
+m_est_noise1 = (((G' * (Cd\G)) + Cm_pri) \ G') * (Cd \ d_noise1);
+m_est_noise2 = (((G' * (Cd\G)) + Cm_pri) \ G') * (Cd \ d_noise2);
+
+figure(5); subplot(1,2,1);
+image(X(1,:),Y(:,1),reshape(m_est_noise1,11,11),"CDataMapping",'scaled'); title('SD = 0.1'); clim([-0.1,0.3]); colorbar();
+hold on;
+plot(sta_x,sta_y,"rx");
+for i = 1:size(putanje_x,1)
+    p = plot(putanje_x(i,:),putanje_y(i,:),'r');
+end
+hold off;
+
+subplot(1,2,2);
+image(X(1,:),Y(:,1),reshape(m_est_noise2,11,11),"CDataMapping",'scaled'); title('SD = 1'); clim([-0.1,0.3]); colorbar();
+hold on;
+plot(sta_x,sta_y,"rx");
+for i = 1:size(putanje_x,1)
+    p = plot(putanje_x(i,:),putanje_y(i,:),'r');
+end
+hold off;
+
+%q2.7
+%{
+    The retrieved structure becomes fuzzier as noise is added to the travel times. As the standard deviation approaches 1, the structure becomes entirely hidden by the noise. THe first regions of the structure to become affected by noise are areas that are relatively underdetermined by crossing travel time paths. Areas that are more overdetermined, such as the line at (0.7,0.9), can be retrieved even with a little bit of noise. Too much noise still destroys the structure in these areas as well.
+%}
+
+%q2.8
+Cm_pri = eye(size(G,2));
+R = (((G' * (Cd\G)) + Cm_pri) \ G') * (Cd \ G);
+%{
+    Trace = 105.8134. This means that the resolution is not perfect (121), but it is not terrible.
+%}
+
+%2.9
+check1 = checkerboard(1,6,6) > 0.5;
+check1 = reshape(check1(1:end-1,1:end-1),121,1);
+
+check2 = checkerboard(2,3,3) > 0.5;
+check2 = reshape(check2(1:end-1,1:end-1),121,1);
+
+test1 = R * check1;
+test2 = R * check2;
+
+figure(6);
+subplot(2,2,1);imagesc(X(1,:),Y(:,1),reshape(check1,11,11));
+subplot(2,2,2);imagesc(X(1,:),Y(:,1),reshape(check2,11,11));
+subplot(2,2,3);imagesc(X(1,:),Y(:,1),reshape(test1,11,11));
+hold on;
+plot(sta_x,sta_y,"rx");
+for i = 1:size(putanje_x,1)
+    p = plot(putanje_x(i,:),putanje_y(i,:),'r');
+end
+hold off;
+subplot(2,2,4);imagesc(X(1,:),Y(:,1),reshape(test2,11,11));
+hold on;
+plot(sta_x,sta_y,"rx");
+for i = 1:size(putanje_x,1)
+    p = plot(putanje_x(i,:),putanje_y(i,:),'r');
+end
+hold off;
+
+%{
+    Once again, the pattern of resolution follows the density of path intersections. At the top of the image, where time delay paths are less dense, the structure is poorly resolved, but toward the bottom of the image, the structure is well resolved because there are many intersecting time delay paths. This pattern is more apparent for the smaller checkerboard size, but it also appears in the large checkerboard size.
+%}
--- MP6/vig_mp6.m
+++ MP6/vig_mp6.m
@@ -38,68 +38,68 @@ hold off;
 
 
 %q2.1
-f = linspace(0,0.1,1000);
-figure(2);
-spectrogram(c,2048,1024,f,20,'yaxis'); title('Raw Spectrogram');
+% f = linspace(0,0.1,1000);
+% figure(2);
+% spectrogram(c,2048,1024,f,20,'yaxis'); title('Raw Spectrogram');
 
-%q2.2
-c_dtr = detrend(c_filt);
-figure(3); hold on;
-plot(t,c_filt,'DisplayName','Raw');title('Part 2 Signal');xlabel('Time (s)'); ylabel('Counts');
-plot(t,c_dtr,'DisplayName','Detrended');
-legend();
-hold off;
-figure(4);
-spectrogram(c_dtr,2048,1024,f,20,'yaxis');
-%{
-    Our signal was shifted up so that it is centered around zero and it is completly flat except for the amplitude of the signal. The spectrogram no longer has a frequency offset in the beginning. Otherwise, the shapes are the same.
-%}
+% %q2.2
+% c_dtr = detrend(c_filt);
+% figure(3); hold on;
+% plot(t,c_filt,'DisplayName','Raw');title('Part 2 Signal');xlabel('Time (s)'); ylabel('Counts');
+% plot(t,c_dtr,'DisplayName','Detrended');
+% legend();
+% hold off;
+% figure(4);
+% spectrogram(c_dtr,2048,1024,f,20,'yaxis');
+% %{
+%     Our signal was shifted up so that it is centered around zero and it is completly flat except for the amplitude of the signal. The spectrogram no longer has a frequency offset in the beginning. Otherwise, the shapes are the same.
+% %}
 
-%q2.3
-%{
-    There is a low frequency signal early on in the spectrogtram, from roughly 0-10 minutes. This feature abprutly changes at ~10 minutes to include moderate amplitude signals of higher freqeuncies. Then, at ~28 minutes, a large amplitude signal is seen that curves to later times at higher frequencies. This feature starts with low frequencies from ~10-30 Hz and ends with higherfrequencies from ~40-60 Hz. This is the result of lower frequency waves arriving before higher freqeuncy waves.
-%}
+% %q2.3
+% %{
+%     There is a low frequency signal early on in the spectrogtram, from roughly 0-10 minutes. This feature abprutly changes at ~10 minutes to include moderate amplitude signals of higher freqeuncies. Then, at ~28 minutes, a large amplitude signal is seen that curves to later times at higher frequencies. This feature starts with low frequencies from ~10-30 Hz and ends with higherfrequencies from ~40-60 Hz. This is the result of lower frequency waves arriving before higher freqeuncy waves.
+% %}
 
-%q2.4
-%{
-    Yes, the speed of Rayleigh Wave propagation is directly related to their period. That is, low frequency, long period waves are faster and thus arrive before high frequency, short period waves, which are slower. Small period = small speed.
-%}
+% %q2.4
+% %{
+%     Yes, the speed of Rayleigh Wave propagation is directly related to their period. That is, low frequency, long period waves are faster and thus arrive before high frequency, short period waves, which are slower. Small period = small speed.
+% %}
 
-%q2.5
-%{
-    Depths of maximum sensitivity = 1/3 * wavelength = 1/3 *velocity*period
-    3.9*100/3 = 130km
-    3.9*80/3 = 104km
-    3.9*60/3 = 78km
-    3.9*50/3 = 65km
-    3.9*40/3 = 52km
-    3.9*30/3 = 39km
-    3.9*20/3 = 26km
-%}
+% %q2.5
+% %{
+%     Depths of maximum sensitivity = 1/3 * wavelength = 1/3 *velocity*period
+%     3.9*100/3 = 130km
+%     3.9*80/3 = 104km
+%     3.9*60/3 = 78km
+%     3.9*50/3 = 65km
+%     3.9*40/3 = 52km
+%     3.9*30/3 = 39km
+%     3.9*20/3 = 26km
+% %}
 
-%q2.6
-%{
-    Since the fastest waves are the ones with low frequencies (i.e. long periods and the ones that "feel" deep in the Earth), I can infer that Vs increases with depth, since Vs and propagation velocity are directly related. In other words, the waves that feel the deepest move the fastest because Vs is higher at depth. 
-%}
+% %q2.6
+% %{
+%     Since the fastest waves are the ones with low frequencies (i.e. long periods and the ones that "feel" deep in the Earth), I can infer that Vs increases with depth, since Vs and propagation velocity are directly related. In other words, the waves that feel the deepest move the fastest because Vs is higher at depth. 
+% %}
 
-%q2.7
-[b1,a1] = butter(4,0.001,'low');
-[b2,a2] = butter(4,0.003,'low');
-[b3,a3] = butter(4,0.005,'low');
-[b4,a4] = butter(4,0.01,'low');
+% %q2.7
+% [b1,a1] = butter(4,0.001,'low');
+% [b2,a2] = butter(4,0.003,'low');
+% [b3,a3] = butter(4,0.005,'low');
+% [b4,a4] = butter(4,0.01,'low');
 
-cfilt_1 = detrend(filtfilt(b1,a1,c));
-cfilt_2 = detrend(filtfilt(b2,a2,c));
-cfilt_3 = detrend(filtfilt(b3,a3,c));
-cfilt_4 = detrend(filtfilt(b4,a4,c));
+% cfilt_1 = detrend(filtfilt(b1,a1,c));
+% cfilt_2 = detrend(filtfilt(b2,a2,c));
+% cfilt_3 = detrend(filtfilt(b3,a3,c));
+% cfilt_4 = detrend(filtfilt(b4,a4,c));
 
-figure(5); hold on;
-subplot(2,2,1);spectrogram(cfilt_1,2048,1024,f,20,'yaxis');title("N=4, Wn=0.001")
-subplot(2,2,2);spectrogram(cfilt_2,2048,1024,f,20,'yaxis');title("N=4, Wn=0.003")
-subplot(2,2,3);spectrogram(cfilt_3,2048,1024,f,20,'yaxis');title("N=4, Wn=0.005")
-subplot(2,2,4);spectrogram(cfilt_4,2048,1024,f,20,'yaxis');title("N=4, Wn=0.1")
-hold off;
+% figure(5); hold on;
+% subplot(2,2,1);spectrogram(cfilt_1,2048,1024,f,20,'yaxis');title("N=4, Wn=0.001")
+% subplot(2,2,2);spectrogram(cfilt_2,2048,1024,f,20,'yaxis');title("N=4, Wn=0.003")
+% subplot(2,2,3);spectrogram(cfilt_3,2048,1024,f,20,'yaxis');title("N=4, Wn=0.005")
+% subplot(2,2,4);spectrogram(cfilt_4,2048,1024,f,20,'yaxis');title("N=4, Wn=0.1")
+% hold off;
 
-%{
-    Changing Wn, the cutoff frequency for the lowpass filter, affects which frequencies can be seen in the spectrogram. At very low Wn, most of the frequencies in the spectrogram are very low power, but at higher Wn, all the frequencies that are in the signal can be seen at their true power, since they are not being filtered out by the lowpass filter. For example, when Wn = 0.0001, only frequencies below 0.001*20Hz ~ 10mHz could be seen, but at Wn = 0.1*20Hz ~ 200mHz, all of the frequencies in the signal could be seen.
-%}
+% %{
+%     Changing Wn, the cutoff frequency for the lowpass filter, affects which frequencies can be seen in the spectrogram. At very low Wn, most of the frequencies in the spectrogram are very low power, but at higher Wn, all the frequencies that are in the signal can be seen at their true power, since they are not being filtered out by the lowpass filter. For example, when Wn = 0.0001, only frequencies below 0.001*20Hz ~ 10mHz could be seen, but at Wn = 0.1*20Hz ~ 200mHz, all of the frequencies in the signal could be seen.
+% %}
